9aec699c899b80b7f3f1070253311055ae099418
Evan Song <theevansong@gmail.com>
2025-03-15 22:26:19 -0700

    chore: clean

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,35 +1,39 @@
-import { headers } from 'next/headers'
+import { headers } from "next/headers";
 
-import Button from '/src/components/Button/Button'
-import { useTranslation } from '/src/i18n/server'
-import { makeClass } from '/src/utils'
+import Button from "/src/components/Button/Button";
+import { useTranslation } from "/src/i18n/server";
+import { makeClass } from "/src/utils";
 
-import styles from './Footer.module.scss'
+import styles from "./Footer.module.scss";
 
 interface FooterProps {
-  isSmall?: boolean
+  isSmall?: boolean;
 }
 
 const Footer = async ({ isSmall }: FooterProps) => {
-  const { t } = await useTranslation('common')
-  const isRunningInApp = headers().get('referer')?.includes('android-app://fit.crab')
+  const { t } = await useTranslation("common");
+  const isRunningInApp = (await headers())
+    .get("referer")
+    ?.includes("android-app://fit.crab");
 
-  return isRunningInApp
-    ? null // Cannot show external donation link in an Android app
-    : <footer
+  return isRunningInApp ? null : ( // Cannot show external donation link in an Android app
+    <footer
       id="donate" // Required to allow scrolling directly to the footer
       className={makeClass(styles.footer, isSmall && styles.small)}
     >
-      <span>{t('donate.info')}</span>
+      <span>{t("donate.info")}</span>
       <Button
         isSmall
-        title={t('donate.title')}
+        title={t("donate.title")}
         href="https://ko-fi.com/A06841WZ"
         target="_blank"
         rel="noreferrer noopener payment"
-        style={{ whiteSpace: 'nowrap' }}
-      >{t('donate.button')}</Button>
+        style={{ whiteSpace: "nowrap" }}
+      >
+        {t("donate.button")}
+      </Button>
     </footer>
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- frontend/src/i18n/options.ts
+++ frontend/src/i18n/options.ts
@@ -1,14 +1,29 @@
-import { InitOptions } from 'i18next'
+import { InitOptions } from "i18next";
 
-export const fallbackLng = 'en'
-export const defaultNS = 'common'
-export const cookieName = 'i18next'
+export const fallbackLng = "en";
+export const defaultNS = "common";
+export const cookieName = "i18next";
 export const languages = [
   fallbackLng,
-  'en-GB', 'de', 'es', 'fr', 'hi', 'id', 'it', 'ja', 'ko', 'pl', 'pt-PT', 'pt-BR', 'ru',
-] as const
+  "en-GB",
+  "de",
+  "es",
+  "fr",
+  "hi",
+  "id",
+  "it",
+  "ja",
+  "ko",
+  "pl",
+  "pt-PT",
+  "pt-BR",
+  "ru",
+] as const;
 
-export const getOptions = (lng = fallbackLng, ns: InitOptions['ns'] = defaultNS): InitOptions => ({
+export const getOptions = (
+  lng = fallbackLng,
+  ns: InitOptions["ns"] = defaultNS
+): InitOptions => ({
   lng,
   fallbackLng,
   supportedLngs: languages,
@@ -19,90 +34,107 @@ export const getOptions = (lng = fallbackLng, ns: InitOptions['ns'] = defaultNS)
   interpolation: {
     escapeValue: false,
   },
-})
+});
 
 interface LanguageDetails {
   /** Name of the language in it's own language */
-  name: string
+  name: string;
   /** 0: Sunday, 1: Monday */
-  weekStart: 0 | 1
-  timeFormat: '12h' | '24h'
+  weekStart: 0 | 1;
+  timeFormat: "12h" | "24h";
 }
 
-export const languageDetails: Record<typeof languages[number], LanguageDetails> = {
-  'en': { // English (US)
-    name: 'English (US)',
+export const languageDetails: Record<
+  (typeof languages)[number],
+  LanguageDetails
+> = {
+  en: {
+    // English (US)
+    name: "English (US)",
     weekStart: 0,
-    timeFormat: '12h',
+    timeFormat: "12h",
   },
-  'en-GB': { // English (UK)
-    name: 'English (UK)',
+  "en-GB": {
+    // English (UK)
+    name: "English (UK)",
     weekStart: 1,
-    timeFormat: '12h',
+    timeFormat: "12h",
   },
-  'de': { // German
-    name: 'Deutsch',
+  de: {
+    // German
+    name: "Deutsch",
     weekStart: 1,
-    timeFormat: '24h',
+    timeFormat: "24h",
   },
-  'es': { // Spanish
-    name: 'Español',
+  es: {
+    // Spanish
+    name: "Español",
     weekStart: 1,
-    timeFormat: '24h',
+    timeFormat: "24h",
   },
-  'fr': { // French
-    name: 'Français',
+  fr: {
+    // French
+    name: "Français",
     weekStart: 1,
-    timeFormat: '24h',
+    timeFormat: "24h",
   },
-  'hi': { // Hindi
-    name: 'हिंदी',
+  hi: {
+    // Hindi
+    name: "हिंदी",
     weekStart: 1,
-    timeFormat: '12h',
+    timeFormat: "12h",
   },
-  'id': { // Indonesian
-    name: 'Indonesia',
+  id: {
+    // Indonesian
+    name: "Indonesia",
     weekStart: 1,
-    timeFormat: '24h',
+    timeFormat: "24h",
   },
-  'it': { // Italian
-    name: 'Italiano',
+  it: {
+    // Italian
+    name: "Italiano",
     weekStart: 1,
-    timeFormat: '24h',
+    timeFormat: "24h",
   },
-  'ja': { // Japanese
-    name: '日本語',
+  ja: {
+    // Japanese
+    name: "日本語",
     weekStart: 0,
-    timeFormat: '12h',
+    timeFormat: "12h",
   },
-  'ko': { // Korean
-    name: '한국어',
+  ko: {
+    // Korean
+    name: "한국어",
     weekStart: 0,
-    timeFormat: '24h',
+    timeFormat: "24h",
   },
-  'pl': { // Polish
-    name: 'Polskie',
+  pl: {
+    // Polish
+    name: "Polskie",
     weekStart: 1,
-    timeFormat: '12h',
+    timeFormat: "12h",
   },
-  'pt-PT': { // Portuguese (Portugal)
-    name: 'Português (do Portugal)',
+  "pt-PT": {
+    // Portuguese (Portugal)
+    name: "Português (do Portugal)",
     weekStart: 0,
-    timeFormat: '24h',
+    timeFormat: "24h",
   },
-  'pt-BR': { // Portuguese (Brazil)
-    name: 'Português (do Brasil)',
+  "pt-BR": {
+    // Portuguese (Brazil)
+    name: "Português (do Brasil)",
     weekStart: 0,
-    timeFormat: '24h',
+    timeFormat: "24h",
   },
-  'ru': { // Russian
-    name: 'Pусский',
+  ru: {
+    // Russian
+    name: "Pусский",
     weekStart: 1,
-    timeFormat: '24h',
+    timeFormat: "24h",
   },
   // 'zh-CN': { // Chinese
   //   name: '中文',
   //   weekStart: 1,
   //   timeFormat: '12h',
   // },
-}
+};
